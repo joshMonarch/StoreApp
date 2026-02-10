78b4513740159f5fa2cc2080528b97b3c934c94b
joshMonarch <sesitado@gmail.com>
2026-02-10 20:10:44 +0100

    :construction: Starting DbContext configuration

---
+++ Store.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Domain.Entities;
+
+namespace Store.Infrastructure.Persistence
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
