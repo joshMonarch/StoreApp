423d5342bc6750688aeb4d929271075d4e532ef2
joshMonarch <sesitado@gmail.com>
2026-02-10 19:34:06 +0100

    :hammer: Created entities

---
+++ Store.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
