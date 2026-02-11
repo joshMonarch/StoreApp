cf89446efd6930c9b4ebb4251c858e50649bb7f1
joshMonarch <sesitado@gmail.com>
2026-02-11 17:26:18 +0100

    :hammer: Created interfaces for each entity's repository

---
+++ Store.Application/Abstractions/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Store.Application.Abstractions.Commons;
+using Store.Domain.Entities;
+
+namespace Store.Application.Abstractions
+{
+    public interface IProductRepository: ICreateRepository<Product>, IReadRepository<Product>, IUpdateRepository<Product>, IDeleteRepository<Product>
+    {
+    }
+}
---
+++ Store.Application/Abstractions/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Store.Application.Abstractions.Commons;
+using Store.Domain.Entities;
+
+namespace Store.Application.Abstractions
+{
+    public interface IUserRepository: ICreateRepository<User>, IReadRepository<User>, IUpdateRepository<User>, IDeleteRepository<User>
+    {
+    }
+}
