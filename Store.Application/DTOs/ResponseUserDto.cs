9a1bd28edba62306c86b8bb113deebee7aa61db6
joshMonarch <sesitado@gmail.com>
2026-02-19 19:06:10 +0100

    :hammer: Defined DTOs

---
+++ Store.Application/DTOs/ResponseUserDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Store.Application.DTOs
+{
+    public class ResponseUserDto
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ResponseUserDto(int id, string? username, string? password, string? email, DateOnly birthDate, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Email = email;
+            BirthDate = birthDate;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+    }
+}
