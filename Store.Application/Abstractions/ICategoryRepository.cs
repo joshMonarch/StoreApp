0bd862a9ca30fa88d0c41424515b27567921f95b
joshMonarch <sesitado@gmail.com>
2026-02-19 19:01:17 +0100

    :hammer: Declared ExistsAsync method

--- Store.Application/Abstractions/ICategoryRepository.cs
+++ Store.Application/Abstractions/ICategoryRepository.cs
@@ -5,5 +5,6 @@ namespace Store.Application.Abstractions
 {
     public interface ICategoryRepository: ICreateRepository<Category>, IReadRepository<Category>, IUpdateRepository<Category>, IDeleteRepository<Category>
     {
+        Task<bool> ExistsAsync(int id, CancellationToken ct);
     }
 }
