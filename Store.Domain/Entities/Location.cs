80d1b8e38c55d8e3a86b3f616497f758614c5787
joshMonarch <sesitado@gmail.com>
2026-02-23 22:09:07 +0100

    :hammer: Updated Entities

--- Store.Domain/Entities/Location.cs
+++ Store.Domain/Entities/Location.cs
@@ -1,4 +1,6 @@
-﻿namespace Store.Domain.Entities
+﻿using Store.Domain.Commons;
+
+namespace Store.Domain.Entities
 {
     public class Location
     {
@@ -7,5 +9,23 @@
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public List<Address> Addresses { get; set; } = new List<Address> { };
+
+        public Location(string? locationType)
+        {
+            LocationType = locationType;
+        }
+
+        public static Result<Location> Create(string? locationType)
+        {
+            if (string.IsNullOrWhiteSpace(locationType))
+                return Result<Location>.Fail("Location type not found.");
+
+            if (locationType.Length < 4 || locationType.Length > 30)
+                return Result<Location>.Fail("Location type must have 4 to 29 characters.");
+
+            var location = new Location(locationType);
+
+            return Result<Location>.Ok(location);
+        }
     }
 }
